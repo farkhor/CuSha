5924e672bf7921f581c120af5096953e25ee1625
Farzad Khorasani <farkhor@users.noreply.github.com>
2014-10-19 20:12:04 -0700

    Update sfBuild.sh

--- sfBuild.sh
+++ sfBuild.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nvcc entryPoint.cu common/simpleTime.cu csr_src/csr-UserFunctions.cu csr_src/csr-utils.cu csr_src/MTCPU.cu csr_src/VWC.cu cusha_src/cusha-CW.cu cusha_src/cusha-GS.cu cusha_src/cusha-UserFunctions.cu cusha_src/cusha-utils.cu -arch=sm_35 -O3 -rdc=true -o cusha
+nvcc entryPoint.cu common/simpleTime.cu csr_src/csr-utils.cu csr_src/MTCPU.cu csr_src/VWC.cu cusha_src/cusha-CW.cu cusha_src/cusha-GS.cu cusha_src/cusha-utils.cu -arch=sm_35 -O3 -rdc=true -o cusha
