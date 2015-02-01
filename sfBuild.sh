44dd2c3ee84f64ee2149f755ab466bb0f7754642
Farzad Khorasani <farkhor@users.noreply.github.com>
2015-01-31 16:04:57 -0800

    Delete sfBuild.sh

--- sfBuild.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-nvcc entryPoint.cu common/simpleTime.cu csr_src/csr-utils.cu csr_src/MTCPU.cu csr_src/VWC.cu cusha_src/cusha-CW.cu cusha_src/cusha-GS.cu cusha_src/cusha-utils.cu -arch=sm_35 -O3 -rdc=true -o cusha
