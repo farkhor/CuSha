23dd57d06bc05eef05764e102b26456ca6dfcece
Farzad Khorasani <farkhor@users.noreply.github.com>
2015-04-01 14:00:09 -0700

    Added -O3 flag.

--- sfBuild.sh
+++ sfBuild.sh
@@ -1,1 +1,1 @@
-nvcc entry_point.cu common/simpleTime.cu csr_src/csr_format.cu csr_src/vwc_process.cu cusha_src/cusha_format.cu cusha_src/cusha_process.cu cusha_src/find_block_size.cu common/parse_graph.cpp -arch=sm_35 -o cusha
+nvcc entry_point.cu common/simpleTime.cu csr_src/csr_format.cu csr_src/vwc_process.cu cusha_src/cusha_format.cu cusha_src/cusha_process.cu cusha_src/find_block_size.cu common/parse_graph.cpp -O3 -arch=sm_35 -o cusha
